a2d8b67d5b4c783f0e4c8cfbd7e31352996c74fd
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-04-12 23:55:49 +0530

    started the onboard screen

---
+++ Gym/Onboard/OnboardScreen.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardScreen.swift
+//  Gym
+//
+//  Created by shashwat singh on 12/04/25.
+//
+
+import SwiftUI
+//enum OnboardPage: Int, CaseIterable {
+//    case
+//}
+struct OnboardScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardScreen()
+}
